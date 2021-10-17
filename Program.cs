f86ee5ae866746bdb337673dee82da2f5c0d708d
AndreySouza1989 <92690312+AndreySouza1989@users.noreply.github.com>
2021-10-17 16:02:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraBasicaComValidacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+            Console.WriteLine("Olá, bem vindo a Calculadora Básica!\n" + "Escolha o tipo de operação desejada utilizando o Menu e em seguida os operandos: ");
+            Console.WriteLine();
+            
+            Console.WriteLine( "Menu:");
+            Console.WriteLine( "1 - adição; 2 - subtração; 3 - Multiplicação; 4 - Divisão; 5 - Saír.");
+
+            var Calculadora = new Calculadora();
+            var entrada = Calculadora.Menu(1);
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
